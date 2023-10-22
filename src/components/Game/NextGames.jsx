1347a183eae8fda2f18fda19aabda9db5fed89a9
BBocho8 <teambbocho@gmail.com>
2023-10-23 00:21:30 +0200

    test new slider

--- src/components/Game/NextGames.jsx
+++ src/components/Game/NextGames.jsx
@@ -2,35 +2,50 @@ import GamesContainer from "./GamesContainer"
 import { nanoid } from "nanoid"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import Slider from "react-slick"
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules"
 
-const settings = {
-	dots: true,
-	infinite: true,
-	speed: 1000,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	pauseOnDotsHover: true,
-}
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
 
 const NextGames = ({ isResult, isHomepage, games }) => {
 	return (
 		<div className="py-4 px-2 sm:px-0">
 			<h2 className="text-center uppercase">NEXT GAMES</h2>
-			<Slider {...settings}>
+			<Swiper
+				modules={[Navigation, Pagination, A11y, Autoplay]}
+				autoplay={{
+					disableOnInteraction: true,
+				}}
+				speed={1000}
+				loop={true}
+				spaceBetween={16}
+				slidesPerView={1}
+				centeredSlides={true}
+				pagination={{
+					el: ".pagination-div-next",
+					clickable: true,
+					renderBullet: (index, className) => {
+						return '<span class="' + className + '">' + "</span>"
+					},
+				}}
+			>
 				{games.map((game) => {
 					return (
-						<GamesContainer
-							key={nanoid()}
-							isResult={isResult}
-							isHomepage={isHomepage}
-							{...game}
-						/>
+						<SwiperSlide key={nanoid()}>
+							<GamesContainer
+								key={game.id}
+								isResult={isResult}
+								isHomepage={isHomepage}
+								{...game}
+							/>
+						</SwiperSlide>
 					)
 				})}
-			</Slider>
+			</Swiper>
+			<div className="pagination-div-next"></div>
 		</div>
 	)
 }
