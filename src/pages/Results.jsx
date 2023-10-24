e9cd37fe83d994daf7c978f3e6dc1ec4e50a9bb1
BBocho8 <teambbocho@gmail.com>
2023-10-24 10:52:42 +0200

    results x Nextgames

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,22 +1,52 @@
+import { useState } from "react"
 import LastGames from "../components/Game/LastGames"
 import NextGames from "../components/Game/NextGames"
 import { useGlobalContext } from "../context"
 
 const Results = () => {
 	const { games } = useGlobalContext()
+	const [isResultsOpen, setIsResultsOpen] = useState(true)
+
 	if (!games.data) {
 		return <h2>Loading...</h2>
 	}
 	const { prevGames, nextGames } = games.data
 	return (
-		<div>
-			<div className="my-2">
+		<div className="">
+			<div className="flex justify-center items-center gap-x-2">
+				<button
+					className={`btn ${!isResultsOpen && "bg-black"}`}
+					type="button"
+					onClick={() => setIsResultsOpen(true)}
+				>
+					RESULTS
+				</button>
+				<button
+					className={`btn bg-black ${!isResultsOpen && "btn bg-primaryGreen"}`}
+					type="button"
+					onClick={() => setIsResultsOpen(false)}
+				>
+					NEXT GAMES
+				</button>
+			</div>
+
+			{isResultsOpen ? (
+				<div className="my-2">
+					<LastGames isResult={true} isHomepage={false} games={prevGames} />
+				</div>
+			) : (
+				<div className="my-8">
+					<NextGames isResult={false} isHomepage={false} games={nextGames} />
+				</div>
+			)}
+
+			{/* <div className="my-2">
 				<LastGames isResult={true} isHomepage={false} games={prevGames} />
 			</div>
 
 			<div className="my-8">
 				<NextGames isResult={false} isHomepage={false} games={nextGames} />
-			</div>
+			</div> */}
 		</div>
 	)
 }
