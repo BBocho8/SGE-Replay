be036df12b9397528505814deac7e49bccd5568f
BBocho8 <teambbocho@gmail.com>
2023-11-03 15:23:38 +0100

    delete former fetch fn

--- src/utils/fetchGames.js
+++ src/utils/fetchGames.js
@@ -1,31 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
-let myHeaders = new Headers()
-myHeaders.append("x-auth-token", import.meta.env.VITE_GAMES_ACCESS_DE_TOKEN)
-
-let requestOptions = {
-	method: "GET",
-	headers: myHeaders,
-	redirect: "follow",
-}
-
-export async function fetchGames() {
-	try {
-		const fetchData = await fetch(
-			`/api/team/${import.meta.env.VITE_DE_TEAM_ID}`,
-			requestOptions
-		)
-		const response = await fetchData.json()
-		const games = response.data
-		const { prevGames, nextGames, table } = games
-
-		return { games, prevGames, nextGames, table }
-	} catch (error) {
-		console.log(error)
-	}
-}
-
 export const getGames = axios.create({
 	baseURL: import.meta.env.VITE_SGE_DB_URL,
 })
