11c224736156e09f8e226e8b6b490e8605316136
Brice Braquin <teambbocho@gmail.com>
2024-09-19 08:33:08 +0200

    fix: implemented full screen for video

--- src/pages/ReplayDetails.jsx
+++ src/pages/ReplayDetails.jsx
@@ -1,28 +1,28 @@
-import { useParams } from "react-router-dom"
-import { useState } from "react"
-import LoadingSpinner from "../components/LoadingSpinner"
-import { getFormattedDate, getFormattedTime } from "../utils/formatDate"
-import { useGetAllVideos } from "../utils/fetchVideo"
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { getFormattedDate, getFormattedTime } from "../utils/formatDate";
+import { useGetAllVideos } from "../utils/fetchVideo";
 
 const ReplayDetails = () => {
-	const [gamePart, setGamePart] = useState("")
-	const [isResultOpen, setIsResultOpen] = useState(false)
+	const [gamePart, setGamePart] = useState("");
+	const [isResultOpen, setIsResultOpen] = useState(false);
 
-	const { gameID } = useParams()
-	const { isVideosLoading, data } = useGetAllVideos()
-	const { videoGames } = data
+	const { gameID } = useParams();
+	const { isVideosLoading, data } = useGetAllVideos();
+	const { videoGames } = data;
 
 	const game = videoGames?.find((videoGame) => {
 		if (gameID === videoGame?.id) {
-			return videoGame
+			return videoGame;
 		}
-	})
+	});
 	if (isVideosLoading || !game) {
 		return (
-			<div className="flex-center my-4">
+			<div className="my-4 flex-center">
 				<LoadingSpinner />
 			</div>
-		)
+		);
 	}
 
 	const {
@@ -41,7 +41,7 @@ const ReplayDetails = () => {
 		secondHalf2,
 		secondHalf3,
 		fullGame,
-	} = game
+	} = game;
 
 	const gameLinks = {
 		firstHalf1,
@@ -51,35 +51,35 @@ const ReplayDetails = () => {
 		secondHalf2,
 		secondHalf3,
 		fullGame,
-	}
+	};
 
-	const gameLinksArray = Object.keys(gameLinks)
+	const gameLinksArray = Object.keys(gameLinks);
 
 	const result = gameLinksArray.filter((link) => {
 		if (game[link]) {
-			return link
+			return link;
 		}
-	})
+	});
 
 	const handleClick = (res) => {
-		setGamePart(res)
-	}
+		setGamePart(res);
+	};
 
 	const handleResult = () => {
-		setIsResultOpen(!isResultOpen)
-	}
+		setIsResultOpen(!isResultOpen);
+	};
 
 	return (
-		<div className="my-4 mx-auto flex flex-col items-center">
-			<p className="font-semibold text-xl my-2">
-				{homeTeam} <span className="font-bold text-xl">-</span> {awayTeam}
+		<div className="flex flex-col items-center mx-auto my-4">
+			<p className="my-2 text-xl font-semibold">
+				{homeTeam} <span className="text-xl font-bold">-</span> {awayTeam}
 			</p>
 			<p>{competition}</p>
 			<p>
 				<span>{getFormattedDate(date)}</span> -{" "}
 				<span>{getFormattedTime(date)}</span>
 			</p>
-			<div className="flex justify-center items-center gap-x-2 my-2 flex-wrap gap-y-2">
+			<div className="flex flex-wrap items-center justify-center my-2 gap-x-2 gap-y-2">
 				{result.map((res, index) => {
 					return (
 						<button
@@ -90,32 +90,32 @@ const ReplayDetails = () => {
 							{res === "firstHalf1"
 								? "First Half 1"
 								: res === "firstHalf2"
-								? "First Half 2"
-								: res === "firstHalf3"
-								? "First Half 3"
-								: res === "secondHalf1"
-								? "Second Half 1"
-								: res === "secondHalf2"
-								? "Second Half 2"
-								: res === "secondHalf3"
-								? "Second Half 3"
-								: res === "fullGame"
-								? "Full Game"
-								: ""}
+									? "First Half 2"
+									: res === "firstHalf3"
+										? "First Half 3"
+										: res === "secondHalf1"
+											? "Second Half 1"
+											: res === "secondHalf2"
+												? "Second Half 2"
+												: res === "secondHalf3"
+													? "Second Half 3"
+													: res === "fullGame"
+														? "Full Game"
+														: ""}
 						</button>
-					)
+					);
 				})}
 			</div>
 			{!isVideoAvailable && (
 				<p className="text-blue-800">Game is not yet available</p>
 			)}
 			{gamePart && (
-				<>
-					<iframe
-						src={game[gamePart]}
-						className="w-4/5 h-auto aspect-video my-2"
-					></iframe>
-				</>
+				<iframe
+					src={game[gamePart]}
+					allowFullScreen
+					className="w-4/5 h-auto my-2 aspect-video"
+					title="video game"
+				/>
 			)}
 
 			<button className="btn" onClick={handleResult}>
@@ -124,11 +124,11 @@ const ReplayDetails = () => {
 			{isResultOpen && (
 				<p className="text-lg">
 					{homeTeam}
-					<span className="font-bold mt-2 text-xl ">{`   ${homeScore} - ${awayScore}   `}</span>
+					<span className="mt-2 text-xl font-bold ">{`   ${homeScore} - ${awayScore}   `}</span>
 					{awayTeam}
 				</p>
 			)}
 		</div>
-	)
-}
-export default ReplayDetails
+	);
+};
+export default ReplayDetails;
